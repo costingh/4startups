1cec4bf8a913d60456c5bee9aa90ac6e20871591
costin <chgeorghe0702@stud.acs.upb.ro>
2021-11-19 18:21:52 +0200

    updated suport me

--- pages/support-me.js
+++ pages/support-me.js
@@ -5,6 +5,11 @@ import styles from "../styles/SupportMe.module.css";
 import Link from "next/link";
 import SEO from "../components/SEO";
 
+const buyMeACoffeLink = "https://www.buymeacoffee.com/4startups";
+const kofiLink = "https://ko-fi.com/4startups";
+const coindropLink = "https://coindrop.to/for_startups";
+const paypalLink = "https://www.paypal.com/biz/fund?id=U8JLPGC9GBRTG";
+
 function SupportMe() {
   return (
     <div>
@@ -17,7 +22,7 @@ function SupportMe() {
             Sign in with one of available providers. If you have not used the
             platforms listed below, you may need to create an account.
           </p>
-          <Link href="https://www.buymeacoffee.com/4startups">
+          <Link href={buyMeACoffeLink}>
             <div className={styles.firstOption}>
               <div className={styles.badge}>Recommended</div>
               <img
@@ -27,23 +32,24 @@ function SupportMe() {
               <p>Buy Me a Coffee</p>
             </div>
           </Link>
-
-          <div className={styles.option}>
-            <img src="/patreon.png" alt=""></img>
-            <p>Patreon</p>
-          </div>
-          <div className={styles.option}>
-            <img src="/ko-fo.png" alt=""></img>
-            <p>Ko-fi</p>
-          </div>
-          <div className={styles.option}>
-            <img src="/coindrop.png" alt=""></img>
-            <p>Coinbase</p>
-          </div>
-          <div className={styles.option}>
-            <img src="/tipeee.png" alt=""></img>
-            <p>Tipeee</p>
-          </div>
+          <Link href={kofiLink}>
+            <div className={styles.option}>
+              <img src="/ko-fo.png" alt=""></img>
+              <p>Ko-fi</p>
+            </div>
+          </Link>
+          <Link href={coindropLink}>
+            <div className={styles.option}>
+              <img src="/coindrop.png" alt=""></img>
+              <p>Coindrop</p>
+            </div>
+          </Link>
+          <Link href={paypalLink}>
+            <div className={styles.option}>
+              <img src="/paypal.png" alt=""></img>
+              <p>PayPal</p>
+            </div>
+          </Link>
           <p className={styles.muted}>
             Every penny helps to offset the cost of developing, hosting and
             maintaining the site.
